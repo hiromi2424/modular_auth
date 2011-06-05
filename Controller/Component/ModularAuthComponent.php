59d5d38119afe995606d9988259dace23abeca97
hiromi2424 <tanaka2424@gmail.com>
2011-06-06 07:21:20 +0900

    fixed AppModularAuthComponent was avoided

--- Controller/Component/ModularAuthComponent.php
+++ Controller/Component/ModularAuthComponent.php
@@ -2,7 +2,7 @@
 
 App::uses('BaseModularAuthComponent', 'ModularAuth.Controller/Component');
 
-App::uses('AppModularAuthComponent', 'Component');
+App::uses('AppModularAuthComponent', 'Controller/Component');
 if (!class_exists('AppModularAuthComponent')) {
 	App::uses('AppModularAuthComponent', 'ModularAuth.Controller/Component');
 }
